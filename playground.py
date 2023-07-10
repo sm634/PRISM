2f12ea78d22c52ec95bf99254970e63ebc146b63
Mukhia <safmuk01@protiviti.com>
2023-07-10 14:08:43 +0100

    key-phrase extractor init
    Signed-off-by: Mukhia <safmuk01@protiviti.com>

--- playground.py
+++ playground.py
@@ -17,26 +17,7 @@ preprocessed_text2 = [preprocessor.preprocess_text(text) for text in text2]
 print("Extracting key phrases from regulation text 2")
 kp_extractor = Extractor()
 
+# extract key phrases from corpus 1 and find matching documents in corpus 2.
+matching_docs, num_of_matching_kp = kp_extractor.find_kw_matching_docs(preprocessed_text1, preprocessed_text2)
 
-reg2_kp = []
-for doc in preprocessed_text2:
-    reg2_kp.append(kp_extractor.extract_keywords(doc))
-
-
-print("Getting matching key phrases in regulation text 1")
-kp_matched_docs = []
-for i, kp_doc in enumerate(reg2_kp):
-    matched_docs = []
-    for j, doc in enumerate(preprocessed_text1):
-        if any(string in doc for string in kp_doc):
-            count = sum(el in doc for el in kp_doc)
-            matched_docs.append((i, j, count))
-
-    kp_matched_docs.append(matched_docs)
-# # sorted key phrase matching docs.
-kp_matched_docs = [sorted(kp_matched, key=lambda x: x[2], reverse=True) for kp_matched in kp_matched_docs]
-num_kp_matches = sum(len(ls) for ls in kp_matched_docs)
-
-
-print("key phrase matching complete")
 breakpoint()
--- utils/key_phrase_extractor.py
+++ utils/key_phrase_extractor.py
@@ -1,5 +1,5 @@
 import yake
-from typing import List
+from typing import List, Tuple, Iterable
 
 
 class Extractor:
@@ -17,9 +17,30 @@ class Extractor:
                                                       top=num_keywords,
                                                       features=features)
 
-    def extract_keywords(self, text: str) -> List:
+    def __extract_keywords(self, text: str) -> List:
         key_words_list_tuple = self.kw_extractor.extract_keywords(text)
         key_words = [i[0] for i in key_words_list_tuple]
         return key_words
 
+    def find_kw_matching_docs(self, corpus1: Iterable, corpus2: Iterable) -> Tuple:
+        print("Extracting key words from corpus2")
+        key_phrases = [self.__extract_keywords(doc) for doc in corpus2]
+
+        print("Getting matching key phrases in corpus1")
+        kp_matched_docs = []
+        for i, kp_doc in enumerate(key_phrases):
+            matched_docs = []
+            for j, doc in enumerate(corpus1):
+                if any(string in doc for string in kp_doc):
+                    count = sum(el in doc for el in kp_doc)
+                    matched_docs.append((i, j, count))
+
+            kp_matched_docs.append(matched_docs)
+        # # sorted key phrase matching docs.
+        kp_matched_docs = [sorted(kp_matched, key=lambda x: x[2], reverse=True) for kp_matched in kp_matched_docs]
+        print("key phrase matching complete")
+        num_kp_matches = sum(len(ls) for ls in kp_matched_docs)
+        print(f"Number of documents with matching key phrases across corpus 1 and corpus 2: {num_kp_matches}")
+
+        return kp_matched_docs, num_kp_matches
 
